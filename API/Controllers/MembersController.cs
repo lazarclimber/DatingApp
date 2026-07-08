fb2158701b4e483cc4d789396434005e644186ee
lazarbitcube <lazarciricthemaze@hotmail.com>
2026-07-08 14:36:01 +0200

    Added authentication and authorization

--- API/Controllers/MembersController.cs
+++ API/Controllers/MembersController.cs
@@ -1,13 +1,13 @@
 ﻿using API.Data;
 using API.Entities;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
-    [Route("api/[controller]")] // localhost:5001/api/members
-    [ApiController]
-    public class MembersController(AppDbContext context) : ControllerBase
+    [Authorize]
+    public class MembersController(AppDbContext context) : BaseApiController
     {
         [HttpGet]
         public async Task<ActionResult<IReadOnlyList<AppUser>>> GetMembers()
@@ -16,8 +16,8 @@ namespace API.Controllers
 
             return members;
         }
-
-        [HttpGet("{id}")] // localhost:5001/api/members/id
+    
+        [HttpGet("{id}")]
         public async Task<ActionResult<AppUser>> GetMember(string id)
         {
             var member = await context.Users.FindAsync(id);
