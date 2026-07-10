a80f8fabd0f93c969947082ac891a5adbdcbc5e8
lazarbitcube <lazarciricthemaze@hotmail.com>
2026-07-10 16:06:17 +0200

    wip

--- client/src/layout/nav/nav.ts
+++ client/src/layout/nav/nav.ts
@@ -1,10 +1,10 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AccountService } from '../../core/services/account-service';
 import { errorContext } from 'rxjs/internal/util/errorContext';
 
 @Component({
-  selector: 'app-nav',
+  selector: 'app-nav', 
   imports: [FormsModule],
   templateUrl: './nav.html',
   styleUrl: './nav.css',
@@ -12,11 +12,20 @@ import { errorContext } from 'rxjs/internal/util/errorContext';
 export class Nav {
   private accountService = inject(AccountService)
   protected creds: any = {};
+  protected loggedIn = signal(false);
 
   login() {
     this.accountService.login(this.creds).subscribe({
-      next: result => console.log(result),
+      next: result => {
+        console.log(result);
+        this.loggedIn.set(true);
+        this.creds = {};
+      },
       error: error => alert(error.message)
     })
+  }
+
+  logout() {
+    this.loggedIn.set(false);
   }
 }
