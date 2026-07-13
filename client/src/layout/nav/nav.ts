14030e19b37ee815ed5378a592d6e2e9d5bc6a5d
lazarbitcube <lazarciricthemaze@hotmail.com>
2026-07-13 15:44:16 +0200

    finished auth

--- client/src/layout/nav/nav.ts
+++ client/src/layout/nav/nav.ts
@@ -10,15 +10,13 @@ import { errorContext } from 'rxjs/internal/util/errorContext';
   styleUrl: './nav.css',
 })
 export class Nav {
-  private accountService = inject(AccountService)
+  protected accountService = inject(AccountService)
   protected creds: any = {};
-  protected loggedIn = signal(false);
 
   login() {
     this.accountService.login(this.creds).subscribe({
       next: result => {
         console.log(result);
-        this.loggedIn.set(true);
         this.creds = {};
       },
       error: error => alert(error.message)
@@ -26,6 +24,6 @@ export class Nav {
   }
 
   logout() {
-    this.loggedIn.set(false);
+    this.accountService.logout();
   }
 }
