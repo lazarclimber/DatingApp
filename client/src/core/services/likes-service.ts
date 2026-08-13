2bb1432efae49950468b4449ac19182774bd3517
lazarbitcube <lazarciricthemaze@hotmail.com>
2026-08-13 14:32:33 +0200

    messages feature (WIP)

--- client/src/core/services/likes-service.ts
+++ client/src/core/services/likes-service.ts
@@ -1,32 +1,37 @@
 import { inject, Injectable, signal } from '@angular/core';
 import { environment } from '../../environments/environment';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Member } from '../../types/member';
+import { PaginatedResult } from '../../types/pagination';
 
 @Injectable({
   providedIn: 'root',
 })
 export class LikesService {
-	private baseUrl = environment.apiUrl;
-	private http = inject(HttpClient);
-	likeIds = signal<string[]>([]);
+  private baseUrl = environment.apiUrl;
+  private http = inject(HttpClient);
+  likeIds = signal<string[]>([]);
 
-	toggleLike(targetMemberId: string) {
-		return this.http.post(`${this.baseUrl}likes/${targetMemberId}`, {});
-	}
+  toggleLike(targetMemberId: string) {
+    return this.http.post(`${this.baseUrl}likes/${targetMemberId}`, {});
+  }
 
-	getLikes(predicate: string) {
-		return this.http.get<Member[]>(this.baseUrl + 'likes?predicate=' + predicate);
-	}
+  getLikes(predicate: string, pageNumber: number, pageSize: number) {
+    let params = new HttpParams();
+    params = params.append('pageNumber', pageNumber);
+    params = params.append('pageSize', pageSize);
+    params = params.append('predicate', predicate);
 
-	getLikeIds() {
-		return this.http.get<string[]>(this.baseUrl + 'likes/list').subscribe({
-			next: ids => this.likeIds.set(ids)
-		})
-	}
+    return this.http.get<PaginatedResult<Member>>(this.baseUrl + 'likes', { params });
+  }
 
-	clearLikeIdk() {
-		this.likeIds.set([]);
-	}
+  getLikeIds() {
+    return this.http.get<string[]>(this.baseUrl + 'likes/list').subscribe({
+      next: (ids) => this.likeIds.set(ids),
+    });
+  }
 
+  clearLikeIdk() {
+    this.likeIds.set([]);
+  }
 }
